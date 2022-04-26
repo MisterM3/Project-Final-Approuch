07c385460bfae5c959ca63fd050ceb830270fb20
MisterM <510249@student.saxion.nl>
2022-04-26 09:47:29 +0200

    Added a CollectableSystem class

---
+++ Physics Game/Game/CollectableSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+public class CollectableSystem {
+
+
+
+    List<int> stars = new List<int>();
+
+    public CollectableSystem() { }
+
+
+
+    public void CheckStars() { 
+    
+    }
+}
