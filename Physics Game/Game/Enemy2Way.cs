536795160cdfea12f36d2f02581c990aba25f6a6
MisterM <510249@student.saxion.nl>
2022-04-26 12:00:45 +0200

    Enemy moves between two points

--- Physics Game/Game/Enemy2Way.cs
+++ Physics Game/Game/Enemy2Way.cs
@@ -11,6 +11,9 @@ public class Enemy2Way : Ball {
     Vec2 startLine;
     Vec2 endLine;
 
+    Vec2 direction;
+
+    bool toEnd = true;
     public Enemy2Way(int radius, Vec2 pPos, Vec2 startLinePos, Vec2 endLinePos, float speed = 1f) : base(radius, pPos) {
 
 
@@ -19,10 +22,32 @@ public class Enemy2Way : Ball {
 
         position = pPos;
 
+        direction = endLine - startLine;
+
+        velocity = direction.Normalized();
+        velocity *= speed;
+
     }
 
+    public void Update() {
+        Move();
+    }
 
-    public void Move() { 
+    public void Move() {
+
+        Vec2 disToEnd = position - endLine;
+        Vec2 disToStart = position - startLine;
+        if (disToEnd.Length() <= 0.05f || disToStart.Length() <= 0.05f)
+        {
+            Console.WriteLine("wa");
+            velocity *= -1;
+        }
+
+        position += velocity;
+
+        x = position.x;
+        y = position.y;
+        
         
     
     }
