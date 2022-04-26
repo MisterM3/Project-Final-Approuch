83b150fa9e4c66a3ba4542da0faa846f8797235a
MisterM <510249@student.saxion.nl>
2022-04-26 08:10:46 +0200

    Line can now be mirrored when pressing LMB

--- Physics Game/Game/Lines.cs
+++ Physics Game/Game/Lines.cs
@@ -8,9 +8,23 @@ using GXPEngine;
 public class Lines : LineSegment {
 
 
+
     public Lines(Vec2 pStart, Vec2 pEnd) : base(pStart, pEnd) { 
     
     
+    
+    
+    }
+
+    public void Update() {
+
+
+        if (!Input.GetMouseButtonDown(0)) return;
+        Vec2 holdStart = start; 
+        Vec2 holdEnd = end;
+
+        start = new Vec2(end.x, start.y);
+        end = new Vec2(holdStart.x, holdEnd.y);
     
     
     }
