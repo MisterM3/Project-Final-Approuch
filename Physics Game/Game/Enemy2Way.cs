eddd5a541fc89d6d1a2acc7163f104502385e5c4
MisterM <510249@student.saxion.nl>
2022-04-26 11:52:38 +0200

    Add not moving Enemy

---
+++ Physics Game/Game/Enemy2Way.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+public class Enemy2Way : Ball {
+
+
+    Vec2 startLine;
+    Vec2 endLine;
+
+    public Enemy2Way(int radius, Vec2 pPos, Vec2 startLinePos, Vec2 endLinePos, float speed = 1f) : base(radius, pPos) {
+
+
+        startLine = startLinePos;
+        endLine = endLinePos;
+
+        position = pPos;
+
+    }
+
+
+    public void Move() { 
+        
+    
+    }
+
+    
+}
