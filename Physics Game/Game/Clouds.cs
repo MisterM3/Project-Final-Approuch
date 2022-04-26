cc41ec3f8542f8cb8e55c54bd7942777f10b256d
MisterM <510249@student.saxion.nl>
2022-04-26 14:51:40 +0200

    Made a box

--- Physics Game/Game/Clouds.cs
+++ Physics Game/Game/Clouds.cs
@@ -7,13 +7,29 @@ using GXPEngine;
 
 public class Clouds : Pivot {
 
+    LineSegment[] lines = new LineSegment[4];
 
-    public Clouds() : base()
+
+
+    public Clouds(Vec2 pTopLeft, Vec2 pBottomRight, float pRot = 0) : base()
     {
-        LineSegment line = new LineSegment(new Vec2(100, 100), new Vec2(500, 500));
-        rotation = 90;
+
+
+        lines[0] = new LineSegment(pTopLeft, new Vec2(pTopLeft.x, pBottomRight.y));
+        lines[1] = new LineSegment(new Vec2(pTopLeft.x, pBottomRight.y), pBottomRight);
+        lines[2] = new LineSegment(pBottomRight, new Vec2(pBottomRight.x, pTopLeft.y));
+        lines[3] = new LineSegment(new Vec2(pBottomRight.x, pTopLeft.y), pTopLeft);
+
+        if (pRot != 0) {
+
+            for (int i = 0; i < lines.Length; i++) { 
+            
+            }
+        
+        }
+        
         MyGame myGame = ((MyGame)game);
-        myGame.addLine(line);
+        for (int i = 0; i < lines.Length; i++) myGame.addLine(lines[i]);
     }
 
     public void Update() {
--- Physics Game/Game/MyGame.cs
+++ Physics Game/Game/MyGame.cs
@@ -162,7 +162,7 @@ public class MyGame : Game
 
 			case 3: // Level2 
 				currentScene = 3;
-				Clouds cloud = new Clouds();
+				Clouds cloud = new Clouds(new Vec2(100, 100), new Vec2(200, 200));
 				AddChild(cloud);
 
 
