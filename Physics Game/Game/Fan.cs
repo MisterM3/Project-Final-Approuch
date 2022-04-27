a6da4e565c99573cc45bd38587334bd9b763edac
MisterM <510249@student.saxion.nl>
2022-04-27 10:21:08 +0200

    Added base Fan class

---
+++ Physics Game/Game/Fan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+public class Fan : Sprite {
+
+    public Fan(Vec2 Pos, int pWidth, int pHeight, float pRot, float pPower) : base("colors.png") { 
+    
+        width = pWidth;
+        height = pHeight;
+        SetOrigin(width / 2, height / 2);
+        rotation = pRot;
+
+        Vec2 force = Vec2.GetUnitVectorDeg(pRot);
+        force *= pPower;
+    
+    }
+
+
+    public void Update() { 
+    
+    
+    }
+}
