69a66f2084bde7ebe97932cfa0cbb31eed6b9e55
MisterM <510249@student.saxion.nl>
2022-04-28 12:17:16 +0200

    Added BallCaps

--- Physics Game/Game/MovablePlatform.cs
+++ Physics Game/Game/MovablePlatform.cs
@@ -16,6 +16,11 @@ public class MoveableLine : LineSegment
     Vec2 center;
     Vec2 velocity;
     Vec2 position;
+
+    Ball ballCaps;
+    Ball ballCaps2;
+
+    MyGame myGame;
     public MoveableLine(Vec2 StartLine, Vec2 EndLine, Vec2 moveFromCenter) : base(StartLine, EndLine) {
 
 
@@ -23,6 +28,14 @@ public class MoveableLine : LineSegment
         end = EndLine;
         moveAround = moveFromCenter;
         center = (EndLine - StartLine)/2.0f;
+
+
+        ballCaps = new Ball(0, StartLine, moving: false);
+        ballCaps2 = new Ball(0, EndLine, moving: false);
+
+        myGame = ((MyGame)game);
+        myGame.addMover(ballCaps);
+        myGame.addMover(ballCaps2);
 
         leftBound = center - moveAround;
         rightBound = center + moveAround;
@@ -37,9 +50,12 @@ public class MoveableLine : LineSegment
         end += velocity;
         start += velocity;
 
+        ballCaps.position += velocity;
+        ballCaps2.position += velocity;
+        
 
 
-        base.Check();
+      
         float distance = (leftBound - rightBound).Length();
         if ((center - leftBound).Length() >= distance || ((center - rightBound).Length() >= distance)) velocity *= -1;
 
