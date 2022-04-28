4d7923a14159b4369385545029ffa239f03c65bc
MisterM <510249@student.saxion.nl>
2022-04-28 11:53:38 +0200

    Made platform hitable from one side

--- Physics Game/Game/MovablePlatform.cs
+++ Physics Game/Game/MovablePlatform.cs
@@ -9,13 +9,40 @@ using GXPEngine;
 public class MoveableLine : LineSegment
 {
 
+    
+    Vec2 leftBound;
+    Vec2 rightBound;
     Vec2 moveAround;
     Vec2 center;
+    Vec2 velocity;
+    Vec2 position;
     public MoveableLine(Vec2 StartLine, Vec2 EndLine, Vec2 moveFromCenter) : base(StartLine, EndLine) {
 
-        moveAround = moveFromCenter;
 
-      
+        start = StartLine;
+        end = EndLine;
+        moveAround = moveFromCenter;
+        center = (EndLine - StartLine)/2.0f;
+
+        leftBound = center - moveAround;
+        rightBound = center + moveAround;
+        velocity = leftBound.Normalized();
+
+        
+
+    }
+
+    public void Update() {
+        center += velocity;
+        end += velocity;
+        start += velocity;
+
+
+
+        base.Check();
+        float distance = (leftBound - rightBound).Length();
+        if ((center - leftBound).Length() >= distance || ((center - rightBound).Length() >= distance)) velocity *= -1;
+
 
     }
 
