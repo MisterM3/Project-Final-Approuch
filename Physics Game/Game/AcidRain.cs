19efda8a05bcf3720e6196d80f20f65aec7cdfa7
MisterM <510249@student.saxion.nl>
2022-04-28 13:25:07 +0200

    Added base class

---
+++ Physics Game/Game/AcidRain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GXPEngine;
+
+public class AcidRain : Sprite {
+
+
+    public AcidRain() : base("colors.png") { 
+    
+    }
+}
