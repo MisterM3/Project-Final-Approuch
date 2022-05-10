6e0b0e3fd9b9b1d26a25e9a4e6a8c8126641e393
MisterM <510249@student.saxion.nl>
2022-05-10 11:50:39 +0200

    Fixed sound bug with endcircle

--- Physics Game/Game/Z.Game/z.Mechanics/ShapeMechanic/CircleMechanic.cs
+++ Physics Game/Game/Z.Game/z.Mechanics/ShapeMechanic/CircleMechanic.cs
@@ -8,7 +8,6 @@ using GXPEngine;
 public class CircleMechanic : Sprite
 {
     protected MyGame myGame;
-    Sound collectStar = new Sound("starsCollectTestSound.wav", false, false);
     public CircleMechanic(Vec2 pPos, int pRadius) : base("circle.png")
     {
         SetOrigin(width / 2, height / 2);
@@ -34,7 +33,7 @@ public class CircleMechanic : Sprite
 
                 if (relPos.Length() < (width / 2) + mover.radius)
                 {
-                    collectStar.Play(false, 0, 0.7f);
+   
                     InCircle(mover, relPos);
                     InCircle();
                 }
