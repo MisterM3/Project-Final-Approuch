45093929cc291b20af782d673a3b5c45ffab6427
MisterM <510249@student.saxion.nl>
2022-05-12 21:48:04 +0200

    FINAL FINAL

--- Physics Game/Game/ParticleSystem.cs
+++ Physics Game/Game/ParticleSystem.cs
@@ -19,23 +19,23 @@ public class ParticleSystem : Pivot {
 
     public void Boom(Vec2 pPos, int size = 1, float pRunTime = 1f, float veloctiy = 5.0f, string overlay = null) {
 
-        ////for (int i = 0; i < particles; i++) {
-        ////    Particle ball = new Particle(10, new Vec2(400,300));
-        ////    ball.velocity = new Vec2(Utils.Random(-3.0f, 3.0f), Utils.Random(-3.0f, 3.0f));
-        ////    parent.AddChild(ball);
-        ////}
-
-        //for (int i = 0; i < particles; i++)
-        //{
-        //    Particle ball = new Particle(size, pPos, spriteName: overlay);
-
-        //    ball.velocity = Vec2.RandomUnitVector();
-        //    ball.velocity *= Utils.Random(0, veloctiy);
-
-        //    //ball.velocity = new Vec2(Utils.Random(-3.0f, 3.0f), Utils.Random(-3.0f, 3.0f));
-        //    Scene scene = SceneManager.instance.GetActiveScene();
-        //    scene.AddChild(ball);
+        //for (int i = 0; i < particles; i++) {
+        //    Particle ball = new Particle(10, new Vec2(400,300));
+        //    ball.velocity = new Vec2(Utils.Random(-3.0f, 3.0f), Utils.Random(-3.0f, 3.0f));
+        //    parent.AddChild(ball);
         //}
+
+        for (int i = 0; i < particles; i++)
+        {
+            Particle ball = new Particle(size, pPos, spriteName: overlay);
+
+            ball.velocity = Vec2.RandomUnitVector();
+            ball.velocity *= Utils.Random(0, veloctiy);
+
+            //ball.velocity = new Vec2(Utils.Random(-3.0f, 3.0f), Utils.Random(-3.0f, 3.0f));
+            Scene scene = SceneManager.instance.GetActiveScene();
+            scene.AddChild(ball);
+        }
     }
 
     public void Cone(Vec2 pPos, float middleDeg, float turnDeg, int size = 1, float pRunTime = 1f, float velocity = 5.0f, Sprite overlay = null) {
