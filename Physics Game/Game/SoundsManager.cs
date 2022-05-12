2465deb6beceaf08fa1402664e747c327950b73c
MisterM <510249@student.saxion.nl>
2022-05-12 14:19:21 +0200

    Added hover and click

--- Physics Game/Game/SoundsManager.cs
+++ Physics Game/Game/SoundsManager.cs
@@ -9,16 +9,34 @@ public class SoundManager : Pivot
 {
 
     public Sound backgroundSound;
-    public SoundChannel channel;
+    SoundChannel channel;
 
+    SoundChannel hover;
     bool switched = false;
+
+    public bool hoverBool = false;
+    Sound[] cloud = new Sound[8];
+
+    Sound click;
     public SoundManager() {
 
- //       backgroundSound = new Sound("cloudsBG.mp3", true);
-  //      backgroundSound.Play();
+        //       backgroundSound = new Sound("cloudsBG.mp3", true);
+        //      backgroundSound.Play();
+
+        click = new Sound("Sounds/Buttons/click.wav");
+        LoadSounds();
+        Sound hoverSound = new Sound("Sounds/Buttons/hover.wav", true);
+
+       // hoverSound.Play();
+        if (hover == null) hover = hoverSound.Play();
+        hover.Volume = 0;
+
+
     }
 
-
+    void LoadSounds() {
+        for (int i = 0; i < 8; i++) cloud[i] = new Sound("Sounds/CloudCol/collision " + i.ToString() + ".wav");
+    }
 
     public void SwitchBGSound(string soundswitch)
     {
@@ -29,7 +47,7 @@ public class SoundManager : Pivot
         {
             switched = true;
             backgroundSound = new Sound(soundswitch);
-
+            
         }
         else if (channel.Volume < 1) channel.Volume += 0.5f;
 
@@ -37,6 +55,10 @@ public class SoundManager : Pivot
 
     }
 
+    void Update() {
+        if (hoverBool) HoverButtonStart();
+        else HoverButtonEnd();
+    }
 
     public void ShootSFX() {
 
@@ -54,14 +76,11 @@ public class SoundManager : Pivot
     public void CloudSFX()
     {
 
-        int i = Utils.Random(1, 9);
+        int i = Utils.Random(0, 8);
 
-        string sound = "Sounds/CloudCol/collision " + i.ToString() + ".wav";
+        
 
-        Console.WriteLine(sound);
-        Sound shoot = new Sound(sound);
-
-        shoot.Play();
+        cloud[i].Play();
 
     }
 
@@ -75,6 +94,24 @@ public class SoundManager : Pivot
         Sound shoot = new Sound(sound);
 
         shoot.Play();
+        
+    }
+
+    public void ClickSFX() {
+
+        click.Play();
+    }
+
+    
+    public void HoverButtonStart() {
+
+        if (hover.Volume < 2) hover.Volume += 0.1f;
+        hoverBool = false;
+        Console.WriteLine(hover.Volume);
+    }
+    public void HoverButtonEnd() {
+        if (hover.Volume > 0) hover.Volume -= 0.1f;
+    
     }
 
 }
